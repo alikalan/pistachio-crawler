47e5491127f812ecb051eb5562d5dd3a5e3df1b8
Ali Kalantarnia <ali.kalantarnia@mail.de>
2024-06-12 10:13:36 +0200

    WIP: shadow root issue solved, storefinder layer not working

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -10,20 +10,19 @@ import re
 import time
 
 def set_up_driver(url):
-
     chromeOptions = webdriver.ChromeOptions()
     chromeOptions.add_argument("--headless")
-    chromeOptions.add_argument("--no-sandbox")
-    chromeOptions.add_argument("--disable-setuid-sandbox")
+    chromeOptions.add_argument("--start-maximized")
     chromeOptions.add_argument("--disable-dev-shm-usage")
     chromeOptions.add_argument("--disable-extensions")
     chromeOptions.add_argument("--disable-gpu")
-    chromeOptions.add_argument("start-minimized")
-    chromeOptions.add_argument("disable-infobars")
-    chromeOptions.add_argument("--remote-debugging-port=9222")
+    chromeOptions.add_argument("--window-size=1920x1080")
+    chromeOptions.add_argument("--disable-blink-features=AutomationControlled")
+    chromeOptions.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chromeOptions.add_experimental_option('useAutomationExtension', False)
 
     service = Service(executable_path=CHROMEPATH)
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chromeOptions)
     driver.get(url)
 
     return driver
@@ -32,50 +31,64 @@ def crawl(url, plz):
     driver = set_up_driver(url)
 
     try:
-        button_to_open_popup = WebDriverWait(driver, 10).until(
+        parent_div = WebDriverWait(driver, 20000).until(EC.presence_of_element_located((By.ID, "usercentrics-root")))
+        shadowRoot = driver.execute_script("return arguments[0].shadowRoot", parent_div)
+        print("shadowroot found")
+
+        button = WebDriverWait(shadowRoot, 100).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".uc-deny-all")))
+        button.click()
+        print("cookies clicked")
+
+        button_to_open_popup = WebDriverWait(driver, 100).until(
             EC.element_to_be_clickable((By.XPATH, "//button[text()='Verfügbarkeit in einem dm-Markt prüfen']"))
         )
+        print("Verfügbarkeit button found")
         button_to_open_popup.click()
+        print("Verfügbarkeit button clicked")
+        print(driver.page_source)
 
-        # Wait for the input field to be visible in the popup
-        input_field = WebDriverWait(driver, 10).until(
+        input_field = WebDriverWait(driver, 100).until(
             EC.visibility_of_element_located((By.XPATH, "//input[@aria-label='Suche nach PLZ, Straße oder Ort']"))
         )
-        # Enter the zip code
-        input_field.send_keys(plz)  # Replace '12345' with the desired zip code
+        print("plz search bar found")
+        input_field.send_keys(plz)
 
-        # Wait for the search button to be clickable and then click it
-        search_button = WebDriverWait(driver, 10).until(
+        search_button = WebDriverWait(driver, 100).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Suche absenden']"))
         )
         driver.execute_script("arguments[0].click();", search_button)
 
-        # Get the page source after the results are loaded
+        print("plz sent")
+        print("search_button:")
+        print(search_button)
+
+
         time.sleep(7)
         source = driver.page_source
+        print("source:")
+        print(source)
         driver.quit()
         return source
     except Exception as e:
         print(f"An error occurred: {e}")
+        driver.quit()
+        return None
 
 def get_shops(url, plz):
-
     source = crawl(url, plz)
+    if source is None:
+        print("Failed to retrieve the page source.")
+        return {}
 
-    # Parse the page source with BeautifulSoup
     soup = BeautifulSoup(source, 'html.parser')
-
-    # Find all elements with data-dmid="store-teaser"
     store_teasers = soup.find_all(attrs={"data-dmid": "store-teaser"})
 
     shop_dict = {}
-
-    # Print the found elements
     for teaser in store_teasers:
         verf = teaser.find('span', {'data-dmid': 'availability-hint-text-container'}).text.strip()
         if "Momentan verfügbar" in verf:
             address = teaser.find('div', {'data-dmid': 'store-teaser-street'}).text.strip()
-            stueck =  re.search(r'\((\d+ Stück)\)', verf).group(1)
+            stueck = re.search(r'\((\d+ Stück)\)', verf).group(1)
             shop_dict[address] = stueck
 
     return shop_dict
